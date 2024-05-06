64373eed868b5a287e91dd087ce44daff0942564
lilmoon99 <oxygen4ik@bk.ru>
2024-05-06 11:51:24 +0400

    added dependencies to pom.xml

---
+++ lesson_4/hw4/src/main/java/org/moon/Homework.java
@@ -0,0 +1,11 @@
+package org.moon;
+
+/**
+ * Перенести структуру дз третьего урока на JPA:
+ * 1. Описать сущности Student и Group
+ * 2. Написать запросы: Find, Persist, Remove
+ * 3. ... поупражняться с разными запросами ...
+ */
+public class Homework {
+
+}
